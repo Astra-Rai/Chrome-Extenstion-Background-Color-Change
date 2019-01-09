58150db6875b3bbb681747a7188687bf6825f005
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-01-09 01:52:56 -0600

    Added comments to code

--- content.js
+++ content.js
@@ -1,7 +1,9 @@
-console.log('Go chrome extension');
+//code to change the background color of paragraph element to pink
 
+//variable paragraph set to target paragraph element
 let paragraphs = document.getElementsByTagName('p');
-
+//for of loop, interate through code, and targets the paragraph variable
+//for each paragraph element, it will change the background color to pink, #FF00FF
 for (elt of paragraphs) {
 	elt.style['background-color'] = '#FF00FF';
 }
